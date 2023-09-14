d92446eff0f7f8b7d44afe7d22a5a434fd729505
msi <meibrohimov@gmail.com>
2023-09-14 09:00:49 +0500

    Initial commit

---
+++ lesson.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// main func println
+func main() {
+	// Variables yozilish usulli 1
+	var name = "matn" // string
+	var son = 34      // int
+	var soon = 3.0    // float32
+	var bin = true    // boolean
+
+	// Variables yozilish usulli 2
+	name1 := "matn" // string
+	son1 := 34      // int
+	soon1 := 3.0    // float32
+	bin1 := true    // boolean
+
+	// Variables yozilish usulli 3
+	var my_var string
+	my_var = "test text"
+	var myvar int
+	myvar = 50
+	fmt.Println(myvar)
+	fmt.Println(my_var)
+
+	// chop etish uchun
+	fmt.Println("hello world")
+	fmt.Println(name)
+	fmt.Println(son)
+	fmt.Println(soon)
+	fmt.Println(bin)
+	fmt.Println(name1)
+	fmt.Println(son1)
+	fmt.Println(soon1)
+	fmt.Println(bin1)
+
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := 7
+	m := 6
+	res := m + n
+	fmt.Print(res)
+}
+
+// go build main.go 2 lik ga o'tkazishr
+// ./main 2 lik code ni run qilish
+// go run main.go
