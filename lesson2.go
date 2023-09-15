49fd8beeb983d7a9a76aac29a69885e4be1252ef
msi <meibrohimov@gmail.com>
2023-09-15 08:46:42 +0500

    go constanta variable 🥶

--- lesson2.go
+++ lesson2.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
+// constanta o'zgarmas o'zgaruvchilar
 func main() {
-	var myvar string = "jhon"
-	fmt.Println(myvar)
+
+	// still 1
+	const pi = 3.14
+	// still 2
+	const (
+		a int = 14
+		b int = 34
+	)
+	fmt.Println(pi, a, b)
 }
