ba998466a302ed93868ca4b026646a0a387dd41b
msi <meibrohimov@gmail.com>
2023-09-20 10:22:04 +0500

    go arrays Slices 🥶

--- lesson4.go
+++ lesson4.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println(len(arr1))
 
 	// go Slices orqali array malumotlarni olish
-	myslice := arr1[0:2]
+	myslice := arr1[0:2] // 0 dan 2 gacha malumotlarni olish uchun Slice ishlatilishi
 
 	fmt.Println(myslice)
 
