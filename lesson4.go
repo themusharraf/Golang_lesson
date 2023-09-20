7e6363bb73d5c333fc6c22d377a38ff7efcb2703
msi <meibrohimov@gmail.com>
2023-09-20 09:53:53 +0500

    go arrays  🥶

--- lesson4.go
+++ lesson4.go
@@ -2,8 +2,9 @@ package main
 
 import "fmt"
 
+// Ushbu misolda taxmin qilingan uzunliklarga ega ikkita massiv (arr1 va arr2) e'lon qilinadi:
 func main() {
-	var arr1 = [...]int{5, 6, 7, 8, 9}
+	var arr1 = [...]int{5, 6, 7, 8, 9} // yani bunda [...] berish orqali arraysga n ta malumot yuklashimiz mumkin
 	arr := [...]int{1, 3, 4, 5, 6, 7}
 	fmt.Println(len(arr))
 	fmt.Println(len(arr1))
